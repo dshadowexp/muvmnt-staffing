d4dfc379d66b1a1cf5166db795f3bed0cb8fd2e2
samkofi.appiahkubi <samkofi.appiahkubi@gmail.com>
2026-03-10 15:05:14 +0000

    adding web

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -24,5 +24,27 @@
         run-server = "npm run dev";
       };
     };
+    # NOTE: This is an excerpt of a complete Nix configuration example.
+    # For more information about the dev.nix file in Firebase Studio, see
+    # https://firebase.google.com/docs/studio/customize-workspace
+
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        # The following object sets web previews
+        web = {
+          command = [
+            "npm"
+            "run"
+            "dev"
+          ];
+          manager = "web";
+          cwd = "/home/user/muvmnt-staffing/web";
+          # Optionally, specify a directory that contains your web app
+          # cwd = "app/client";
+        };
+      };
+    };
   };
 }
